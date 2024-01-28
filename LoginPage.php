aa2b47caea546b6c6f624e2a399381b1841994b3
Anis Umairah <anisumairaha@student.usm.my>
2024-01-28 17:05:00 +0800

    login signup

--- LoginPage.php
+++ LoginPage.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
 
 <!doctype html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="initial-scale=1, width=device-width" />
 
@@ -45,151 +45,149 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" href="./LoginPage.css" />
 
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Krona One:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Krona One:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Cagliostro:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Cagliostro:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Literata:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Literata:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Changa One:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Changa One:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Changa:wght@400;500&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Changa:wght@400;500&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Caudex:wght@700&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Caudex:wght@700&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Cutive Mono:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Cutive Mono:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Cairo:wght@500;700&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Cairo:wght@500;700&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Judson:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Judson:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Literata:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Literata:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Moul:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Moul:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Changa:wght@400;500&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Changa:wght@400;500&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Archivo Black:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Archivo Black:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Cambo:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Cambo:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Fira Sans:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Fira Sans:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Kotta One:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Kotta One:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Changa One:wght@400&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Changa One:wght@400&display=swap"
     />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Caudex:wght@700&display=swap"
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Caudex:wght@700&display=swap"
     />
-  </head>
-  <body>
-    <div class="login-page">
-      <img class="rect-pass-icon1" alt="" src="./public/rect-email@2x.png" />
-
-      <div class="background-kiri"></div>
-
-      <div class="login1">Login</div>
-      <div class="background">
+</head>
+<body>
+<div class="login-page">
+    <img class="rect-pass-icon1" alt="" src="./public/rect-email@2x.png" />
+    <div class="background-kiri"></div>
+    <h1 class="log-in">Log in</h1>
+    <div class="background">
         <img
-          class="image-removebg-preview-5-1"
-          alt=""
-          src="./public/imageremovebgpreview-5-1@2x.png"
-        />
-      </div>
-      <div class="logo-psj">
+                class="image-removebg-preview-5-1"
+                alt=""
+                src="./public/imageremovebgpreview-5-1@2x.png"/>
+    </div>
+    <div class="logo-psj">
         <img
-          class="psj-transparent-32"
-          alt=""
-          src="./public/psj-transparent-3@2x.png"
+                class="psj-transparent-32"
+                alt=""
+                src="./public/psj-transparent-3@2x.png"
         />
-      </div>
-      <div class="tulisan-psj">
+    </div>
+    <div class="tulisan-psj">
         <h1 class="projek-sedekah-jariah3">PROJEK SEDEKAH JARIAH</h1>
-      </div>
-      
-      <img class="rect-mykad-icon" alt="" src="./public/rect-email@2x.png" />
+    </div>
 
-      <form method="POST" action="LoginPage.php">
-      <h3 class="nombor-mykad">Nombor MyKad</h3>
-      <div class="input-mykad">
-        <input class="xxxxxxxxx" type="text" name = "mykad" required/>
-      </div>
+    <img class="rect-mykad-icon" alt="" src="./public/rect-email@2x.png" />
 
-      <h3 class="password1">Password</h3>
-      <input class="input-password" type="password" name = "pass" required/>
+    <form method="POST" action="LoginPage.php">
+        <h3 class="nombor-mykad">Nombor MyKad</h3>
+        <div class="input-mykad">
+            <input class="xxxxxxxxx" type="text" name = "mykad" required/>
+        </div>
 
-      <button type = "submit" value= "Log in" name = "submit" class="button-login1">
-        <div class="button-login-child" id="rectangle"></div>
-        <h1 class="log-in">Log in</h1>
-      </button>
-</form>
+        <h3 class="password1">Password</h3>
+        <input class="input-password" type="password" name = "pass" required/>
 
-      <button class="sign-up" id="signUp">
+        <button type="submit" name="submit" class="button-login1" value = "login">
+            <div class="button-login-child" id="rectangle"></div>
+            <h1 class="login1">Login</h1>
+        </button>
+
+    </form>
+
+    <button class="sign-up" id="signUp">
         <span class="sign-up-txt-container">
           <span class="dont-have-an">Don’t have an account? </span>
           <span class="sign-up1">Sign Up</span>
         </span>
-      </button>
-      <div class="button-forgot-pass">
+    </button>
+    <div class="button-forgot-pass">
         <div class="forgot-password">Forgot Password</div>
-      </div>
-      
+    </div>
+
 </div>
 
-    <script>
-      // var rectangle = document.getElementById("rectangle");
-      // if (rectangle) {
-      //   rectangle.addEventListener("click", function (e) {
-      //     // Please sync "User Profile" to the project
-      //     window.location.href = "welcome.php";
-      //   });
-      // }
+<script>
+    // var rectangle = document.getElementById("rectangle");
+    // if (rectangle) {
+    //   rectangle.addEventListener("click", function (e) {
+    //     // Please sync "User Profile" to the project
+    //     window.location.href = "welcome.php";
+    //   });
+    // }
 
-      var signUp = document.getElementById("signUp");
-      if (signUp) {
+    var signUp = document.getElementById("signUp");
+    if (signUp) {
         signUp.addEventListener("click", function (e) {
-          window.location.href = "./SignUpPage.php";
+            window.location.href = "./SignUpPage.php";
         });
-      }
-    </script>
-  </body>
+    }
+</script>
+</body>
 </html>
